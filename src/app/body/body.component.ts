da6f740da12c2a5ad2b371d97e5c6e19fca8df1c
Banoth Prashanth <112512655+PrashanthChotu@users.noreply.github.com>
2024-06-15 19:09:07 +0530

    task9

---
+++ src/app/body/body.component.ts
@@ -0,0 +1,83 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-body',
+  templateUrl: './body.component.html',
+  styleUrl: './body.component.css'
+})
+export class BodyComponent {
+  movie1=[
+    {
+      url:"https://wvvv.ibomma.gift/te-a2ftv/salaar-part-1-ceasefire-2023-g8jmp-telugu-movie-watch-online.html",
+      movie: "SALAAR",
+      imgurl:"https://stat4.bollywoodhungama.in/wp-content/uploads/2023/12/Salaar-322x465.jpg",
+      year:2024,
+
+    },
+    {
+      url:"https://wvvv.ibomma.gift/te-a2ftv/baahubali-the-beginning-2015-telugu-movie-watch-online.html",
+      movie: "BAAHUBALI",
+      year:2015,
+      imgurl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTNFE3mNUcT6Sq3xW8pVVB379vV6RqZmzR2g&s",
+      
+    },{
+      url:"https://wvvv.ibomma.gift/te-a2ftv/baahubali-2-the-conclusion-2017-telugu-movie-watch-online.html",
+      movie: "BAAHUBALI 2",
+      year:2017,
+      imgurl:"https://rukminim2.flixcart.com/image/850/1000/kzygpzk0/poster/r/q/s/large-movie-bahubali-2-posters-on-large-print-36x24-inches-original-imagbukjsjtksjsg.jpeg?q=90&crop=false",
+      
+    },{
+      url:"https://wvvv.ibomma.gift/te-a2ftv/v-telugu-2020-telugu-movie-watch-online.html",
+      movie: "V",
+      year:2020,
+      imgurl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTeM9LDcx3P1gjk8fzhOFdKk_My-PSS5xd-A&s",
+      
+    },{
+      url:"https://wvvv.ibomma.gift/te-a2ftv/wanted-2011-telugu-movie-watch-online.html",
+      movie: "wanted",
+      year:2011,
+      imgurl:"https://m.media-amazon.com/images/M/MV5BNjU5OTBlNjktOGNlOS00ZTFmLTlmNDEtNTk2MjhhYTRlOTQ3XkEyXkFqcGdeQXVyNDY5MTUyNjU@._V1_.jpg",
+      
+    },{
+      url:"https://wvvv.ibomma.gift/te-a2ftv/scam-1992-2000-telugu-telugu-movie-watch-online.html",
+      movie: "scam 1992",
+      year:2000,
+      imgurl:"https://m.media-amazon.com/images/M/MV5BODQ2ZGFjZTctOTk1ZS00YmEyLWFlYTMtYTViZjIyY2FhYTg5XkEyXkFqcGdeQXVyMTAyMTE1MDA1._V1_.jpg",
+      
+    },{
+      url:"https://mov.ibomma.market/ai-n2cdl/bujji-and-bhairava-2024-prt4y-telugu-movie-watch-online.html",
+      movie: "B&B (bujji & Bhairava)",
+      year:2024,
+      imgurl:"https://m.media-amazon.com/images/M/MV5BOWQxNDVlMDUtNDBlMi00MzdjLWJlOGMtZDVkZGI0YmFhNmE0XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+      
+    },{
+      url:"https://mov.ibomma.market/ai-n2cdl/rathnam-2024-hjn2q-telugu-movie-watch-online.html",
+      movie: "Rathnam",
+      year:2024,
+      imgurl:"https://www.businessoftollywood.com/wp-content/uploads/2023/12/Rathnam-movie-poster.jpg",
+      
+    },{
+      url:"https://wvvv.ibomma.gift/te-a2ftv/eagle-2024-wtc2h-telugu-movie-watch-online.html",
+      movie: "eagle",
+      year:2024,
+      imgurl:"https://images.filmibeat.com/ph-big/2024/02/eagle_170745242130.jpg",
+      
+    },{
+      url:"https://wvvv.ibomma.gift/te-a2ftv/tillu-square-2024-ycl5r-telugu-movie-watch-online.html",
+      movie: "tillu 2",
+      year:2024,
+      imgurl:"https://img.republicworld.com/rimages/tillus-square-1704037473528.webp?tr=w-600",
+      
+    },
+  ]
+  
+
+  count=0;
+  fun1(){
+    this.count++
+    
+
+  }
+
+
+}
